0ca24ed0433718776fbbd5ba1daf07a07c5e4eda
Travis Patterson <masslessparticle@gmail.com>
2018-01-24 20:08:13 -0700

    clients can send messages from console. Server sends messages to all clients

--- client/main.go
+++ client/main.go
@@ -4,52 +4,68 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"sync"
-	"time"
-
+	"os"
 	"github.com/masslessparticle/chat/chat"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"log"
+	"bufio"
+	"strings"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:10000", grpc.WithInsecure())
-	if err != nil {
-		panic(err)
-	}
+	username := os.Getenv("USER")
+	stream, conn := connectToChatServer(username)
 	defer conn.Close()
 
-	client := chat.NewChatClient(conn)
-	stream, err := client.StartChat(context.Background())
+	go printReceivedMessages(stream) //TODO: There should be a done chan
+
+	userPrefix := fmt.Sprintf("%s: ", username)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Err() != nil {
+			log.Fatalf("scanner failed: %s", scanner.Err())
+		}
+
+		message := strings.TrimPrefix(scanner.Text(), userPrefix)
+		stream.Send(&chat.ChatMessage{
+			Msg: message,
+		})
+	}
+}
+
+func connectToChatServer(username string) (chat.Chat_StartChatClient, *grpc.ClientConn) {
+	conn, err := grpc.Dial("localhost:10000", grpc.WithInsecure())
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to connect to chatserver: %s", err)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		for {
-			chatMessage := &chat.ChatMessage{
-				User: "user1",
-				Msg:  "Hello world",
-			}
+	client := chat.NewChatClient(conn)
+	ctx := metadata.NewOutgoingContext(
+		context.Background(),
+		metadata.Pairs("username", username),
+	)
 
-			stream.Send(chatMessage)
+	stream, err := client.StartChat(ctx)
+	if err != nil {
+		log.Fatalf("Failed to start chat: %s", err)
+	}
 
-			msg, err := stream.Recv()
-			if err == io.EOF {
-				fmt.Println("Disconnect")
-				wg.Done()
-				return
-			}
-			if err != nil {
-				panic(err)
-			}
+	fmt.Println("Connected to chat server")
+	return stream, conn
+}
 
-			fmt.Printf("%s: %s\n", msg.GetUser(), msg.GetMsg())
-			time.Sleep(500 * time.Millisecond)
+func printReceivedMessages(stream chat.Chat_StartChatClient) {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			fmt.Println("Disconnect")
+			return
 		}
-	}()
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Println("Chat Client Started")
-	wg.Wait()
+		fmt.Printf("%s: %s\n", msg.GetUser(), msg.GetMsg())
+	}
 }
--- server/main.go
+++ server/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
+	"sync"
 
 	"github.com/masslessparticle/chat/chat"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type ChatServer struct {
+	lock     sync.Mutex
+	users    map[string]chat.Chat_StartChatServer
+	messages chan *chat.ChatMessage
 }
 
 func (c *ChatServer) StartChat(stream chat.Chat_StartChatServer) error {
+	md, ok := metadata.FromIncomingContext(stream.Context())
+	if !ok {
+		return errors.New("no metadat sent with request")
+	}
+
+	fmt.Println(md)
+
+	username := md["username"][0]
+	c.addClient(username, stream)
+
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
@@ -23,16 +39,30 @@ func (c *ChatServer) StartChat(stream chat.Chat_StartChatServer) error {
 			return err
 		}
 
-		fmt.Printf("%s: %s\n", in.GetUser(), in.GetMsg())
-
 		outMsg := &chat.ChatMessage{
-			User: in.GetUser(),
+			User: username,
 			Msg:  in.GetMsg(),
 		}
-		stream.Send(outMsg)
-
+		c.messages <- outMsg
 	}
 	return nil
+}
+
+func (c *ChatServer) addClient(username string, stream chat.Chat_StartChatServer) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.users[username] = stream
+}
+
+func (c *ChatServer) startSender() {
+	for message := range c.messages {
+		c.lock.Lock()
+		for _, stream := range c.users {
+			stream.Send(message)
+		}
+		c.lock.Unlock()
+	}
 }
 
 func main() {
@@ -42,8 +72,14 @@ func main() {
 		panic(err)
 	}
 
+	chatServer := &ChatServer{
+		users:    make(map[string]chat.Chat_StartChatServer),
+		messages: make(chan *chat.ChatMessage, 5000),
+	}
+	go chatServer.startSender()
+
 	grpcServer := grpc.NewServer()
-	chat.RegisterChatServer(grpcServer, &ChatServer{})
+	chat.RegisterChatServer(grpcServer, chatServer)
 	fmt.Println("Chat Server Started")
 
 	grpcServer.Serve(ln)
